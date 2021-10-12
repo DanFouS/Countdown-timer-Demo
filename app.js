2003e8bf343d961b6e1408fa59cfe229bd42c523
DanFouS <rouzmfawer@gmail.com>
2021-10-12 21:27:34 +0200

    date

--- app.js
+++ app.js
@@ -21,3 +21,18 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelector(".deadline-format h4");
+
+let futureDate = new Date(2021, 1, 1, 11, 30, 0);
+// console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const seconds = futureDate.getSeconds();
+
+
+giveaway.textContent = `Giveaway ends on ${year} ${hours}:${minutes}:${seconds}am`;
